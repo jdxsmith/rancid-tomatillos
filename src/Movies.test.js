b05c69ca4590b2de94b71fbcd5ca9e3867991525
Jordon Smith <jordon00smith@gmail.com>
2020-12-03 19:16:46 -0700

    Add unit test to Movies to check that all movies are rendered

--- src/Movies.test.js
+++ src/Movies.test.js
@@ -2,9 +2,20 @@ import React from 'react'
 import { fireEvent, render, screen } from '@testing-library/react'
 import Movies from './Movies.js'
 import '@testing-library/jest-dom'
+import movieData from './movieData.js'
 
 describe('Movies', () => {
     it('should render correctly', () => {
-        
+        const mockFindPoster = jest.fn()
+        render(<Movies 
+            movies={movieData.movies}
+            findPoster={mockFindPoster}
+        />)
+
+        const moviesArray = screen.getAllByAltText('movie poster')
+
+        moviesArray.forEach(movie => {
+            expect(movie).toBeInTheDocument()
+        })
     })
 })
