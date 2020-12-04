2faf69a3e3c3a04ae12d24cfd14875fd058e9639
Jordon Smith <jordon00smith@gmail.com>
2020-12-03 19:16:08 -0700

    Add unit test to Poster to check if Poster is rendered

--- src/Poster.test.js
+++ src/Poster.test.js
@@ -5,11 +5,24 @@ import '@testing-library/jest-dom'
 
 describe('Poster', () => {
     it('should render correctly', () => {
+        const mockFindPoster = jest.fn()
+        render(<Poster 
+            image={`https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg`}
+            id={694919}
+            findPoster={mockFindPoster}
+        />)
+
+        const moviePoster = screen.getByAltText('movie poster')
+
+        expect(moviePoster).toBeInTheDocument()
+    })
+
+    it('should call findPoster when a poster is clicked', () => {
         //setup
         //render a poster with key-value pairs for image, id, and findPoster
         //create a variable to represent each image
         //get all images by using ByAltText
-        const mockFindPoster = jest.fn();
+        const mockFindPoster = jest.fn()
         render(<Poster 
             image={`https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg`}
             id={694919}
