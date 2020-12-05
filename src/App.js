3b242a6e10587dc50dd3aff5909de3a1bf3145f0
Cameron Romo <cromo1225@gmail.com>
2020-12-05 12:43:51 -0700

    Refactor to use endpoint for api-call and use componenDidMount to promise data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
-import movieData from './movieData';
-import Movies from './Movies';
+import { movieData } from './apiCalls.js';
+import Movies from './Movies.js';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      movies: movieData.movies,
+      movies: [],
     }
+  }
+
+  componentDidMount() {
+    movieData()
+    .then(movies => this.setState({ movies: movies.movies }))
+    .catch(err => console.error(err.message))
   }
 
   findPoster = (id) => {
@@ -17,20 +23,23 @@ class App extends Component {
     this.setState({ movies: selectedMovie });
   }
 
-  render() {
-    const movieInfo = 
+  setMovieInfo = () => {
+    return(
       <section className='movie-info'>
         <p>{this.state.movies[0].title}</p>
         <p>Average Rating: {this.state.movies[0].average_rating}</p>
         <p>Release Date: {this.state.movies[0].release_date}</p>
         <button onClick={() => window.location.reload()}>Go Back</button>
       </section>
-    
+    )
+  }
+
+  render() {
     return (
       <section>
         <h1 className='page-title'>Rancid Tomatillos</h1>
         <Movies movies={this.state.movies} findPoster={this.findPoster}/>
-        {this.state.movies.length === 1 && movieInfo}
+        {this.state.movies.length === 1 &&  this.setMovieInfo()}
       </section>
     )
   }
