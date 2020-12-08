e8b75bb87d591df2f16ffc1aa9b55c87166aec68
Cameron Romo <cromo1225@gmail.com>
2020-12-08 16:20:00 -0700

    Add link with path and poster id

--- src/Poster.js
+++ src/Poster.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './Poster.css';
+import { Link } from 'react-router-dom';
 
 const Poster = ({image, id, findPoster, isSinglePoster}) => {
     return (
         <article>
-            <img alt='movie poster' className={isSinglePoster === true ? 'locked-poster' : 'poster'} onClick={() => findPoster(id)} src={image}></img> 
+            <Link to={ `/${id}` } activeStyle={{ color: 'black' }}>
+                <img alt='movie poster' className={isSinglePoster === true ? 'locked-poster' : 'poster'} onClick={() => findPoster(id)} src={image}></img> 
+            </Link>
         </article>
     )
 }
