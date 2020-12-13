1393a66392aeffcdc9156f8a6d6e868e5def924c
Jordon Smith <jordon00smith@gmail.com>
2020-12-13 13:47:10 -0700

    Incorporate a beforeEach for asynchronous test files

--- src/App.test.js
+++ src/App.test.js
@@ -6,177 +6,91 @@ import { movieData , movieDetailsById } from './apiCalls.js';
 import { MemoryRouter } from "react-router-dom";
 jest.mock('./apiCalls.js');
 
-describe('App', () => {
-    it('should render correctly', async () => {
-      movieData.mockResolvedValueOnce({movies: [
-        {
-          id: 1,
-          poster_path: "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
-          backdrop_path: "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
-          title: 'Money Plane',
-          average_rating: 6.666666666666667,
-          release_date: "2020-09-29",
-        }
-      ]});
-      movieDetailsById.mockResolvedValue({movie: {
+describe.only('App', () => {
+  beforeEach(() => {
+    movieData.mockResolvedValue({movies: [
+      {
+        id: 1,
+        poster_path: "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
+        backdrop_path: "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
+        title: 'Money Plane',
+        average_rating: 6.666666666666667,
+        release_date: "2020-09-29",
+      },
+      {
         id: 2,
-        title: "Mulan",
         poster_path: "https://image.tmdb.org/t/p/original//aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg",
         backdrop_path: "https://image.tmdb.org/t/p/original//zzWGRw277MNoCs3zhyG3YmYQsXv.jpg",
-        release_date: "2020-09-04",
-        overview: "When the Emperor of China issues a decree that one man per family must serve in the Imperial Chinese Army to defend the country from Huns, Hua Mulan, the eldest daughter of an honored warrior, steps in to take the place of her ailing father. She is spirited, determined and quick on her feet. Disguised as a man by the name of Hua Jun, she is tested every step of the way and must harness her innermost strength and embrace her true potential.",
-        genres: [
-          "Action",
-          "Adventure",
-          "Drama",
-          "Fantasy"
-        ],
-        budget: 200000000,
-        revenue: 57000000,
-        runtime: 115,
-        tagline: "",
-        average_rating: 5.2727272727272725
-      }})
-      render(
-        <MemoryRouter>
-          <App />
-        </MemoryRouter>
-      );
-
-      const movieInfo = await waitFor(() => screen.getByAltText('movie poster'));
-      const moviesSection = await waitFor(() => screen.getByText('Movies'));
-
-      expect(movieInfo).toBeInTheDocument();
-      expect(moviesSection).toBeInTheDocument();
-    })
-
-    it('should have a Go Back button that returns the user to the home page', async () => {
-      movieData.mockResolvedValueOnce({movies: [
-        {
-          id: 1,
-          poster_path: "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
-          backdrop_path: "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
-          title: 'Money Plane',
-          average_rating: 6.666666666666667,
-          release_date: "2020-09-29",
-        },
-        {
-          id: 2,
-          poster_path: "https://image.tmdb.org/t/p/original//aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg",
-          backdrop_path: "https://image.tmdb.org/t/p/original//zzWGRw277MNoCs3zhyG3YmYQsXv.jpg",
-          title: "Mulan",
-          average_rating: 4.909090909090909,
-          release_date: "2020-09-04"
-        },
-        {
-          id: 3,
-          poster_path: "https://image.tmdb.org/t/p/original//uOw5JD8IlD546feZ6oxbIjvN66P.jpg",
-          backdrop_path: "https://image.tmdb.org/t/p/original//x4UkhIQuHIJyeeOTdcbZ3t3gBSa.jpg",
-          title: "Rogue",
-          average_rating: 5.428571428571429,
-          release_date: "2020-08-20"
-        },
-      ]});
-      movieDetailsById.mockResolvedValue({movie: {
-        id: 2,
         title: "Mulan",
-        poster_path: "https://image.tmdb.org/t/p/original//aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg",
-        backdrop_path: "https://image.tmdb.org/t/p/original//zzWGRw277MNoCs3zhyG3YmYQsXv.jpg",
-        release_date: "2020-09-04",
-        overview: "When the Emperor of China issues a decree that one man per family must serve in the Imperial Chinese Army to defend the country from Huns, Hua Mulan, the eldest daughter of an honored warrior, steps in to take the place of her ailing father. She is spirited, determined and quick on her feet. Disguised as a man by the name of Hua Jun, she is tested every step of the way and must harness her innermost strength and embrace her true potential.",
-        genres: [
-          "Action",
-          "Adventure",
-          "Drama",
-          "Fantasy"
-        ],
-        budget: 200000000,
-        revenue: 57000000,
-        runtime: 115,
-        tagline: "",
-        average_rating: 5.2727272727272725
-      }})
-      render(
-        <MemoryRouter>
-          <App />
-        </MemoryRouter>
-      );
+        average_rating: 4.909090909090909,
+        release_date: "2020-09-04"
+      },
+      {
+        id: 3,
+        poster_path: "https://image.tmdb.org/t/p/original//uOw5JD8IlD546feZ6oxbIjvN66P.jpg",
+        backdrop_path: "https://image.tmdb.org/t/p/original//x4UkhIQuHIJyeeOTdcbZ3t3gBSa.jpg",
+        title: "Rogue",
+        average_rating: 5.428571428571429,
+        release_date: "2020-08-20"
+      },
+    ]});
+    movieDetailsById.mockResolvedValue({movie: {
+      id: 2,
+      title: "Mulan",
+      poster_path: "https://image.tmdb.org/t/p/original//aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg",
+      backdrop_path: "https://image.tmdb.org/t/p/original//zzWGRw277MNoCs3zhyG3YmYQsXv.jpg",
+      release_date: "2020-09-04",
+      overview: "When the Emperor of China issues a decree that one man per family must serve in the Imperial Chinese Army to defend the country from Huns, Hua Mulan, the eldest daughter of an honored warrior, steps in to take the place of her ailing father. She is spirited, determined and quick on her feet. Disguised as a man by the name of Hua Jun, she is tested every step of the way and must harness her innermost strength and embrace her true potential.",
+      genres: [
+        "Action",
+        "Adventure",
+        "Drama",
+        "Fantasy"
+      ],
+      budget: 200000000,
+      revenue: 57000000,
+      runtime: 115,
+      tagline: "",
+      average_rating: 5.2727272727272725
+    }})
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+  })
 
-      const movieInfo = await waitFor(() => screen.getAllByAltText('movie poster'));
+  it('should render correctly', async () => {
+    const movieInfo = await waitFor(() => screen.getAllByAltText('movie poster'));
+    const moviesSection = await waitFor(() => screen.getByText('Movies'));
 
-      movieInfo.forEach(movie => {
-        expect(movie).toBeInTheDocument();
-      })
-
-      fireEvent.click(movieInfo[0]);
-
-      const goBackBtn = screen.getByText('Go Back');
-
-      expect(goBackBtn).toBeInTheDocument();
-
-      fireEvent.click(goBackBtn);
-
-      expect(goBackBtn).not.toBeInTheDocument();
+    movieInfo.forEach(movie => {
+      expect(movie).toBeInTheDocument();
     })
+    expect(moviesSection).toBeInTheDocument();
+  })
 
-    it('should display movie info', async () => {
-      movieData.mockResolvedValue({movies: [
-        {
-          id: 1,
-          poster_path: "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
-          backdrop_path: "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
-          title: 'Money Plane',
-          average_rating: 6.666666666666667,
-          release_date: "2020-09-29",
-        },
-        {
-          id: 2,
-          poster_path: "https://image.tmdb.org/t/p/original//aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg",
-          backdrop_path: "https://image.tmdb.org/t/p/original//zzWGRw277MNoCs3zhyG3YmYQsXv.jpg",
-          title: "Mulan",
-          average_rating: 4.909090909090909,
-          release_date: "2020-09-04"
-        },
-        {
-          id: 3,
-          poster_path: "https://image.tmdb.org/t/p/original//uOw5JD8IlD546feZ6oxbIjvN66P.jpg",
-          backdrop_path: "https://image.tmdb.org/t/p/original//x4UkhIQuHIJyeeOTdcbZ3t3gBSa.jpg",
-          title: "Rogue",
-          average_rating: 5.428571428571429,
-          release_date: "2020-08-20"
-        },
-      ]});
-      movieDetailsById.mockResolvedValue({movie: {
-        id: 2,
-        title: "Mulan",
-        poster_path: "https://image.tmdb.org/t/p/original//aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg",
-        backdrop_path: "https://image.tmdb.org/t/p/original//zzWGRw277MNoCs3zhyG3YmYQsXv.jpg",
-        release_date: "2020-09-04",
-        overview: "When the Emperor of China issues a decree that one man per family must serve in the Imperial Chinese Army to defend the country from Huns, Hua Mulan, the eldest daughter of an honored warrior, steps in to take the place of her ailing father. She is spirited, determined and quick on her feet. Disguised as a man by the name of Hua Jun, she is tested every step of the way and must harness her innermost strength and embrace her true potential.",
-        genres: [
-          "Action",
-          "Adventure",
-          "Drama",
-          "Fantasy"
-        ],
-        budget: 200000000,
-        revenue: 57000000,
-        runtime: 115,
-        tagline: "",
-        average_rating: 5.2727272727272725
-      }})
-      render(
-        <MemoryRouter>
-          <App />
-        </MemoryRouter>
-      );
+  it('should have a Go Back button that returns the user to the home page', async () => {
+    const movieInfo = await waitFor(() => screen.getAllByAltText('movie poster'));
+    
+    fireEvent.click(movieInfo[0]);
 
-      const movieInfo = await waitFor(() => screen.getAllByAltText('movie poster'));
-      
-      fireEvent.click(movieInfo[0]);
+    const goBackBtn = await waitFor(() => screen.getByText('Go Back'));
 
-      const runtime = await waitFor(() => screen.getByText('Runtime: 115 minutes'));
+    expect(goBackBtn).toBeInTheDocument();
 
-      expect(runtime).toBeInTheDocument();
-    })
+    fireEvent.click(goBackBtn);
+
+    expect(goBackBtn).not.toBeInTheDocument();
+  })
+
+  it('should display movie info', async () => {
+    const movieInfo = await waitFor(() => screen.getAllByAltText('movie poster'));
+    
+    fireEvent.click(movieInfo[0]);
+
+    const runtime = await waitFor(() => screen.getByText('Runtime: 115 minutes'));
+
+    expect(runtime).toBeInTheDocument();
+  })
 })
--- src/Movies.test.js
+++ src/Movies.test.js
@@ -1,20 +1,16 @@
 import React from 'react'
-import { fireEvent, render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import Movies from './Movies.js'
 import '@testing-library/jest-dom'
 import movieData from './movieData.js'
 import { MemoryRouter } from 'react-router-dom'
-// import { movieData } from './apiCalls.js';
-// jest.mock('./apiCalls');
 
 describe('Movies', () => {
     it('should render correctly', () => {
-        const mockFindPoster = jest.fn()
         render(
             <MemoryRouter>
                 <Movies 
                     movies={movieData.movies}
-                    findPoster={mockFindPoster}
                 />
             </MemoryRouter>
         )
